d7a849287dc0f3e836f34256ffbb21982c213748
Soumya Ranjan Sahoo <48331344+soumyaranjan-01@users.noreply.github.com>
2020-10-09 19:32:42 +0530

    Update sub.cpp

--- sub.cpp
+++ sub.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
-int x,y;
-cout << x << " - " << y << x-y << endl;
-return 0;
+int main(){
+   int x,y;
+   cin >> x >> y;
+   cout << x << " - " << y << x-y << endl;
+   return 0; 
+}
